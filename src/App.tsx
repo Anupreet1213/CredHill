e71497c01e57a95e003400e11e2d30adc64d66cb
Anupreet Srivastava <93524594+Anupreet1213@users.noreply.github.com>
2023-06-14 12:54:44 +0530

    Update sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Hero from "./components/Hero/Hero";
+// import Hero from "./components/Hero/Hero";
 import "./styles.css";
-import Testimonials from "./components/Testimonials/Testimonials";
-import WhyInvoicer from "./components/WhyInvoicer/WhyInvoicer";
-import Navbar from "./components/Navbar/Navbar";
-import Features from "./components/Features/Features";
-import AboutUs from "./components/AboutUs/AboutUs";
-import Test from "./Test";
+// import Testimonials from "./components/Testimonials/Testimonials";
+// import WhyInvoicer from "./components/WhyInvoicer/WhyInvoicer";
+// import Navbar from "./components/Navbar/Navbar";
+// import Features from "./components/Features/Features";
+// import AboutUs from "./components/AboutUs/AboutUs";
+// import Test from "./Test";
 import Test2 from "./Test2";
 
 function App() {
