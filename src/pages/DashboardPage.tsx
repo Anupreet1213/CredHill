754ed95db7ed5768d6a65b1117c73018bcb51050
Anupreet Srivastava <anupreet.2003@gmail.com>
2023-08-07 21:44:10 +0530

    Bug fix

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
-import MiniDrawer from "../components/dashboard/MiniDrawer";
-import Dashboard from "../components/dashboard/Dashboard";
+// import MiniDrawer from "../components/dashboard/MiniDrawer";
+// import Dashboard from "../components/dashboard/Dashboard";
 import Dashboard2 from "../components/dashboard/Dashboard2";
 
 const DashboardPage = () => {
