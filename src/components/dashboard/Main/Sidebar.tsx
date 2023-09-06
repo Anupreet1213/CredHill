d770e9ccfcc8bb24956f5b4ce03c48eb489b6d73
AmanApT <amanasstudent@gmail.com>
2023-09-06 23:08:07 +0530

    Warning Removal

--- src/components/dashboard/Main/Sidebar.tsx
+++ src/components/dashboard/Main/Sidebar.tsx
@@ -3,7 +3,7 @@ import "./Sidebar.css";
 import HomeIcon from "@mui/icons-material/Home";
 import EqualizerIcon from "@mui/icons-material/Equalizer";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import InsightsIcon from '@mui/icons-material/Insights';
+// import InsightsIcon from '@mui/icons-material/Insights';
 
 interface DashboardProps {
   open: boolean;
