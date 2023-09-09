4bd3e0791fe91025ed8e003688a3435d01436fed
AmanApT <amanasstudent@gmail.com>
2023-09-09 22:24:02 +0530

    Graph + QOD implemented

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -5,8 +5,8 @@ import MobileSidebar from "../components/dashboard/Main/MobileSidebar";
 import { useTheme } from "../contexts/ThemeContext";
 import lifetime_data_image from "./image 1.png";
 import ReceiptIcon from "@mui/icons-material/Receipt";
-
-// import Navbar from "../components/dashboard/Main/Navbar";
+import axios from 'axios';
+import Chart from "react-apexcharts";
 
 const DashboardPage: React.FC = () => {
   const [open, setOpen] = useState<boolean>(true);
@@ -35,14 +35,60 @@ const DashboardPage: React.FC = () => {
     };
   }, []);
 
-  // ---------------------
-  // const [open2, setOpen2] = useState<boolean>(true);
-
-  // const handleClick = () => {
-  //   setOpen2(!open2);
-  // };
-
   const { isDarkMode, toggleTheme } = useTheme();
+
+  // const { quote, loading_quote, error } = UseRandomQuote();
+
+
+
+  const [chartOptions, setChartOptions] = useState({
+    chart: {
+      id: "basic-area"
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996]
+    }
+  });
+
+  const [chartSeries, setChartSeries] = useState([
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60]
+    }
+  ]);
+
+
+
+
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://quotes15.p.rapidapi.com/quotes/random/',
+    headers: {
+      'X-RapidAPI-Key': '0ef2dc056cmshebff74aae91ff26p17dd9cjsn62c90cd53d63',
+      'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+    }
+  };
+
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.request(options);
+         const new_quote = (response.data.content);
+        setQuote(new_quote)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+
+
 
   return (
     <div className="dashboard_main">
@@ -107,8 +153,27 @@ const DashboardPage: React.FC = () => {
             </div>
           </div>
 
-          <div className="dashboard_graph"></div>
-          <div className="dashboard_QOD"></div>
+          <div className="dashboard_graph">
+
+{/* 
+                <div className="dashboard_graph_chart">
+
+          <Chart
+            // style={{maxHeight:"50%"}}  
+            options={chartOptions}
+            series={chartSeries}
+            type="area"
+            width="500"
+            />
+            </div> */}
+
+
+          </div>
+          <div className="dashboard_QOD">
+            <p className="dashboard_QOD_heading">Quote of the Day</p>
+                <p className="dashboard_QOD_quote">{quote}</p>
+                
+          </div>
           <div className="dashboard_main_part"></div>
         </div>
       </div>
