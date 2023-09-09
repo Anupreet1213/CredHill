7a1cf2a0fb5b6d3ea4b318917a05cb04550789ea
AmanApT <amanasstudent@gmail.com>
2023-09-09 23:59:03 +0530

    Dashboard Main Top Functioning complete

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -5,8 +5,12 @@ import MobileSidebar from "../components/dashboard/Main/MobileSidebar";
 import { useTheme } from "../contexts/ThemeContext";
 import lifetime_data_image from "./image 1.png";
 import ReceiptIcon from "@mui/icons-material/Receipt";
-import axios from 'axios';
-// import Chart from "react-apexcharts";
+import AttachMoneySharpIcon from '@mui/icons-material/AttachMoneySharp';
+import DateRangeSharpIcon from '@mui/icons-material/AttachMoneySharp';
+
+
+import axios from "axios";
+import Chart from "react-apexcharts";
 
 const DashboardPage: React.FC = () => {
   const [open, setOpen] = useState<boolean>(true);
@@ -37,38 +41,49 @@ const DashboardPage: React.FC = () => {
 
   const { isDarkMode, toggleTheme } = useTheme();
 
-  // const { quote, loading_quote, error } = UseRandomQuote();
+  const [chartOptions] = useState({
+    chart: {
+      id: "basic-area",
+      toolbar: {
+        show: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    grid: {
+      show: false,
+    },
+    tooltip:{
+      enabled:false
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996],
+      labels:{
+        show:false
+      }
+    },
+    yaxis:{
+      labels:{
+        show:false
+      }
+    }
+  });
 
-
-
-  // const [chartOptions, setChartOptions] = useState({
-  //   chart: {
-  //     id: "basic-area"
-  //   },
-  //   xaxis: {
-  //     categories: [1991, 1992, 1993, 1994, 1995, 1996]
-  //   }
-  // });
-
-  // const [chartSeries, setChartSeries] = useState([
-  //   {
-  //     name: "series-1",
-  //     data: [30, 40, 45, 50, 49, 60]
-  //   }
-  // ]);
-
-
-
-
-
+  const [chartSeries] = useState([
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60],
+    },
+  ]);
 
   const options = {
-    method: 'GET',
-    url: 'https://quotes15.p.rapidapi.com/quotes/random/',
+    method: "GET",
+    url: "https://quotes15.p.rapidapi.com/quotes/random/",
     headers: {
-      'X-RapidAPI-Key': '0ef2dc056cmshebff74aae91ff26p17dd9cjsn62c90cd53d63',
-      'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "0ef2dc056cmshebff74aae91ff26p17dd9cjsn62c90cd53d63",
+      "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+    },
   };
 
   const [quote, setQuote] = useState("");
@@ -77,18 +92,15 @@ const DashboardPage: React.FC = () => {
     const fetchData = async () => {
       try {
         const response = await axios.request(options);
-         const new_quote = (response.data.content);
-        setQuote(new_quote)
+        const new_quote = response.data.content;
+        setQuote(new_quote);
       } catch (error) {
         console.error(error);
       }
     };
-  
+
     fetchData();
   }, []);
-
-
-
 
   return (
     <div className="dashboard_main">
@@ -119,61 +131,63 @@ const DashboardPage: React.FC = () => {
         >
           <div className="lifetime_data">
             <div className="lifetime_data_content">
-              <p> Lifetime Data</p>
+              <p className="lifetime_data_content_heading"> Lifetime Data</p>
               <div className="lifetime_data_content_grid">
                 <div className="total_invoice">
                   <ReceiptIcon fontSize="large" />
                   <div>
-                    <p>Total Invoice</p>
+                    <p style={{color:"#C0C0C0"}}>Total Invoice</p>
                     <p>0</p>
                   </div>
                 </div>
                 <div className="amount_due">
-                  <ReceiptIcon fontSize="large" />
+                  <AttachMoneySharpIcon fontSize="large" />
                   <div>
-                    <p>Total Invoice</p>
+                    <p style={{color:"#C0C0C0"}}>Amount Due</p>
                     <p>0</p>
                   </div>
                 </div>
                 <div className="invoice_due">
-                  <ReceiptIcon fontSize="large" />
+                  <DateRangeSharpIcon fontSize="large" />
                   <div>
-                    <p>Total Invoice</p>
+                    <p style={{color:"#C0C0C0"}}>Invoice Due</p>
                     <p>0</p>
                   </div>
                 </div>
-                {/* <div className="amount_due"></div>
-                <div className="invoice_due"></div> */}
+               
               </div>
             </div>
 
             <div className="lifetime_data_image">
-              {/* <p>Hellu</p> */}
+             
               <img src={lifetime_data_image} alt="" />
             </div>
           </div>
 
           <div className="dashboard_graph">
-
-{/* 
-                <div className="dashboard_graph_chart">
-
-          <Chart
-            // style={{maxHeight:"50%"}}  
-            options={chartOptions}
-            series={chartSeries}
-            type="area"
-            width="500"
-            />
-            </div> */}
-
-
+            <div className="dashboard_graph_chart">
+            <div className="dashboard_graph_chart_heading">
+                  <AttachMoneySharpIcon fontSize="large" />
+                  <div>
+                    <p>97.5k</p>
+                    <p>Revenue Generated</p>
+                  </div>
+                </div>
+              <div>
+              <Chart
+                options={chartOptions}
+                series={chartSeries}
+                type="area"
+                width="100%"
+                height="100%"
+              />
+              </div>
+            </div>
           </div>
           <div className="dashboard_QOD">
             <p className="dashboard_QOD_heading">Quote of the Day</p>
-                <p className="dashboard_QOD_quote">{quote}</p>
-                
-          </div>
+            <p className="dashboard_QOD_quote">{quote}</p>
+                </div>
           <div className="dashboard_main_part"></div>
         </div>
       </div>
