14108edd2e9ac22cb6438333fa8d2df1d820359f
AmanApT <amanasstudent@gmail.com>
2023-09-13 09:59:35 +0530

    Add click sound on Theme toggle

--- src/components/dashboard/Main/Navbar.tsx
+++ src/components/dashboard/Main/Navbar.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React,{useRef,useState} from 'react'
 import { useTheme } from '../../../contexts/ThemeContext';
 import "./Navbar.css";
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import clickSound from './switch-on.mp3'
+import clickSound2 from './switch-off.mp3'
 
 interface NavbarProps{
     handleDrawerToggle: () => void;
@@ -10,11 +12,30 @@ interface NavbarProps{
 
 const Navbar: React.FC<NavbarProps> = ({handleDrawerToggle}) => {
     const { isDarkMode, toggleTheme } = useTheme();
+
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const [currentSound, setCurrentSound] = useState<number>(1);
+    const audioSources = [clickSound, clickSound2];
+    const toggleSound = () => {
+        setCurrentSound(currentSound === 1 ? 2 : 1);
+    };
+    const playClickSound = () => {
+        if (audioRef.current) {
+            audioRef.current.src = audioSources[currentSound - 1];
+            audioRef.current.play();
+        }
+    };
     return (
         <div
             className={`${isDarkMode ? "" : "light-mode"} dashboard_navbar`}
-            onClick={handleDrawerToggle}
+            onClick={() => {
+                handleDrawerToggle();
+                toggleSound();
+                playClickSound();
+            }}
+            
         >
+              <audio ref={audioRef} src={clickSound}></audio>
             {
                 isDarkMode 
                 ?
