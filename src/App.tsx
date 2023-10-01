ee882289c587e4a36d3733da4e08d30dd5defb51
AmanApT <amanasstudent@gmail.com>
2023-10-01 09:30:56 +0530

    New Loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import LandingPage from "./pages/LandingPage";
 // import Dashboard from './pages/Dashboard';
 import { lazy, Suspense } from "react";
 import ThemeProvider from "./contexts/ThemeContext";
+import "./styles.css";
 
 const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <div className="custom-loader-wrapper">
+              <div className="custom-loader"></div>
+            </div>
+          }
+        >
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
